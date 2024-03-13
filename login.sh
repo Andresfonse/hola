14609e9055f1f03405337e3d711e508c0a167739
Andres Eli <110643161+Andresfonse@users.noreply.github.com>
2024-03-13 13:27:53 -0500

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Navega al directorio del proyecto de ejemplo
-cd /home/deavila747/project/miniproject
+cd miniproject
 
 # Construye la imagen Docker
 docker build -t miniproject .
 
 # Ejecuta el contenedor Docker
-docker run -t -d -p 3088:3088 --name loginrunning miniproject
+docker run -t -d -p 3088:3088 --name holamundo miniproject
 
 # Muestra la lista de contenedores Docker
 docker ps -a
